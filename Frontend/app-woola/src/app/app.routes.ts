a466c8667e9b27ecb66a46d119d02a770b3dae12
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2024-03-29 13:31:47 +0100

    Vista usuarios angular

--- Frontend/app-woola/src/app/app.routes.ts
+++ Frontend/app-woola/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {UserComponent} from './user/user.component'
+
+export const routes: Routes = [
+  {path: '', component: UserComponent}
+];
---
+++ Frontend/app-woola/src/app/user/user.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+import {User} from '../models/user.model'
+import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faUser, faStar } from '@fortawesome/free-solid-svg-icons';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [RouterOutlet,RouterLink, FontAwesomeModule, CommonModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent implements OnInit {
+
+  u?:User;
+  username?:string;
+
+  faGauge=faStar;
+  faBars = faBars;
+  faUser = faUser;
+
+  showme:boolean = true;
+
+  constructor(private userService:UserService, private authService:AuthService ,private router: Router){}
+
+  ngOnInit(): void {
+      this.loadUser();
+  }
+
+  loadUser(){
+    this.userService.getMe().subscribe(
+      response=>{
+        this.u = response;
+        this.username=this.u.username;
+        if(!(this.u.rol === "BASE"))
+          this.router.navigate(['']);
+      },
+      error =>{
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  logout(){
+    this.authService.logOut();
+  }
+
+  toogleSidebar(){
+    this.showme = !this.showme;
+  }
+}
